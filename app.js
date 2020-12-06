9f7961b287ce05f593c2e646cad16617eca0f3bd
notadilnaqvi <notadilnaqvi@gmail.com>
2020-12-07 04:52:38 +0500

    Added ground truth

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ $(document).ready(function(){
             $whiteElo = parseInt($("#whiteElo").val());
             $blackElo = parseInt($("#blackElo").val());
             $result = $("input[type='radio']:checked").val();
-            
-            if ($result == "win") { $result = 1;} 
-            else if ($result == "draw") { $result = 0.5;} 
+
+            if ($result == "win") { $result = 1;}
+            else if ($result == "draw") { $result = 0.5;}
             else {$result = 0;}
 
-            $eloChange = model.predict(tf.tensor([[$whiteElo, $blackElo, $result]]));
+            $predictedEloChange = model.predict(tf.tensor([[$whiteElo, $blackElo, $result]]));
+            $expW = 1 / (1 + 10**(($blackElo - $whiteElo) / 400));
+            $actualEloChange = 10*($result - $expW);
 
-            if(isNaN($eloChange.dataSync()[0])){
-                $("#eloChange").removeClass().addClass("alert alert-danger").text("Invalid input!")
+            if(isNaN($predictedEloChange.dataSync()[0])){
+                $("#eloChange").removeClass().addClass("alert alert-danger").text("Invalid input!");
+                $("#actualEloChange").removeClass().addClass("alert alert-danger").text("Invalid input!")
             } else {
-                $("#eloChange").removeClass().addClass("alert alert-primary").text(($eloChange.dataSync()[0] + $whiteElo).toFixed(1));
+                $("#eloChange").removeClass().addClass("alert alert-primary").text(`Predicted Elo: ${($predictedEloChange.dataSync()[0] + $whiteElo).toFixed(1)}`);
+                $("#actualEloChange").removeClass().addClass("alert alert-success").text(`Ground truth: ${($actualEloChange + $whiteElo).toFixed(1)}`);
             }
         })
     }
